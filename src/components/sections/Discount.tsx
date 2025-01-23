53b2c97b98a73f07fe1af45131ab4fe2e047edda
Bouih32 <enamto.bouih@gmail.com>
2025-01-23 16:39:29 +0000

    feat: enhance Discount section with email subscription form and updated text

--- src/components/sections/Discount.tsx
+++ src/components/sections/Discount.tsx
@@ -1,7 +1,16 @@
 export default function Discount() {
   return (
     <section className="bg-discount bg-cover bg-no-repeat mt-[50px] lg:mt-[70px]  lg:h-[490px] h-[250px]">
-      Discount
+      <div className="text-white space-y-5">
+        <h3 className="lg:text-40sm w-[288px] lg:w-[635px]  font-semibold lg:leading-[52px] text-24sm leading-[31.2px]">
+          Get more discount <br /> Off your order
+        </h3>
+        <p className="">Join our mailing list</p>
+        <div className="">
+          <input type="text" name="" id="" placeholder="Your email address" />
+          <button>Shop Now</button>
+        </div>
+      </div>
     </section>
   );
 }
