1b7bc7a03c4c06f942495b681ac9f3a9b3b229fc
Bouih32 <enamto.bouih@gmail.com>
2025-01-23 16:49:00 +0000

    feat: enhance styling and layout of Discount section for improved user experience

--- src/components/sections/Discount.tsx
+++ src/components/sections/Discount.tsx
@@ -1,11 +1,11 @@
 export default function Discount() {
   return (
-    <section className="bg-discount bg-cover bg-no-repeat mt-[50px] lg:mt-[70px]  lg:h-[490px] h-[250px]">
-      <div className="text-white space-y-5">
+    <section className="bg-discount bg-cover bg-no-repeat mt-[50px] lg:mt-[70px]  lg:h-[490px] h-[250px] flex justify-center items-center text-center lg:text-start">
+      <div className="text-white space-y-5 ">
         <h3 className="lg:text-40sm w-[288px] lg:w-[635px]  font-semibold lg:leading-[52px] text-24sm leading-[31.2px]">
           Get more discount <br /> Off your order
         </h3>
-        <p className="lg:text-20sm text-14sm font-semibold leading-[14px] lg:leading-[20px] ">
+        <p className="lg:text-20sm text-14sm  leading-[14px] lg:leading-[20px] ">
           Join our mailing list
         </p>
         <div className="lg:space-x-[22px] space-x-[15px]">
@@ -16,7 +16,9 @@ export default function Discount() {
             placeholder="Your email address"
             className="lg:p-[22px] rounded-[8px] py-[14px] px-[20px] lg:w-[384px] w-[220px] outline-none"
           />
-          <button className="bg-second">Shop Now</button>
+          <button className="bg-second rounded-[8px] px-6 py-[14px] lg:px-[28px] lg:py-5">
+            Shop Now
+          </button>
         </div>
       </div>
     </section>
