7e9574d34d303a86b3d31c52019e32882a5862ec
Bouih32 <enamto.bouih@gmail.com>
2025-01-23 16:44:43 +0000

    feat: improve styling and layout of email subscription form in Discount section

--- src/components/sections/Discount.tsx
+++ src/components/sections/Discount.tsx
@@ -5,10 +5,18 @@ export default function Discount() {
         <h3 className="lg:text-40sm w-[288px] lg:w-[635px]  font-semibold lg:leading-[52px] text-24sm leading-[31.2px]">
           Get more discount <br /> Off your order
         </h3>
-        <p className="">Join our mailing list</p>
-        <div className="">
-          <input type="text" name="" id="" placeholder="Your email address" />
-          <button>Shop Now</button>
+        <p className="lg:text-20sm text-14sm font-semibold leading-[14px] lg:leading-[20px] ">
+          Join our mailing list
+        </p>
+        <div className="lg:space-x-[22px] space-x-[15px]">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Your email address"
+            className="lg:p-[22px] rounded-[8px] py-[14px] px-[20px] lg:w-[384px] w-[220px] outline-none"
+          />
+          <button className="bg-second">Shop Now</button>
         </div>
       </div>
     </section>
