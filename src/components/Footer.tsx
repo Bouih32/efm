d75e3a32c67c4fee39b8fc72c6c4dd4fff4da9d4
Bouih32 <enamto.bouih@gmail.com>
2025-01-23 17:13:26 +0000

    feat: enhance Footer component with additional social media icons and new layout structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,32 @@
 import Container from "./Container";
-import { FaFacebookSquare } from "react-icons/fa";
+import {
+  FaFacebookSquare,
+  FaInstagram,
+  FaTwitter,
+  FaReddit,
+} from "react-icons/fa";
 import Social from "./Social";
+import Ul from "./Ul";
 
 export default function Footer() {
   return (
     <footer className="bg-second text-white lg:py-[65px] py-10">
       <Container>
-        <section className="flex lg:flexBetween flex-col lg:flex-row">
-          <h2>FurniShop</h2>
-          <div className="">
+        <section className="flex gap-5 lg:flexBetween flex-col lg:flex-row lg:pb-[60px] lg:border-b border-white">
+          <h2 className="font-bold text-24sm ">FurniShop</h2>
+          <div className="flex gap-4 justify-between lg:justify-center">
+            <Social icon={<FaFacebookSquare />} />
+            <Social icon={<FaInstagram />} />
+            <Social icon={<FaTwitter />} />
+            <Social icon={<FaReddit />} />
             <Social icon={<FaFacebookSquare />} />
           </div>
         </section>
-        <section className=""></section>
+        <section className="lg:pt-[64px] pt-[45px] grid lg:flex flexBetween grid-cols-2 gap-y-[65px]">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <Ul key={index} />
+          ))}
+        </section>
       </Container>
     </footer>
   );
---
+++ src/components/Ul.tsx
@@ -0,0 +1,13 @@
+export default function Ul() {
+  return (
+    <div>
+      <ul className="text-14sm space-y-5">
+        <li className="text-18sm">Our Products</li>
+        <li>The Support Suite</li>
+        <li>The Sales Suite</li>
+        <li>Support</li>
+        <li>Guide</li>
+      </ul>
+    </div>
+  );
+}
